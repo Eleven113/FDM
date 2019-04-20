85b99b5476ab98455e99629e14abb95dfa28e753
Eleven113 <minard.thibaut@free.fr>
2019-04-20 12:01:19 +0200

    ID DIVs chronos

--- js/fdm.js
+++ js/fdm.js
@@ -61,7 +61,7 @@ button.addEventListener("click", function () {
     
     let div_timer_btn = document.createElement("div");
     div_timer_btn.id = "timer_btn";
-    div_timer_btn.innerHTML = '<i class="fas fa-play"></i><i class="fas fa-pause"></i><i class="fas fa-step-forward"></i>';
+    div_timer_btn.innerHTML = '<i class="fas fa-play" id="play_btn"></i><i class="fas fa-pause" id="pause_btn"></i><i class="fas fa-step-forward" id="step_fwd_btn"></i>';
     chrono.append(div_timer_btn);
     
 });
@@ -234,3 +234,7 @@ function Timer() {
 }
 
 let timer = new Timer();
+let play_btn = document.getElementById("play_btn");
+let pause_btn = document.getElementById("pause_btn");
+let step_fwd_btn = document.getElementById("step_fwd");
+console.log(play_btn);
